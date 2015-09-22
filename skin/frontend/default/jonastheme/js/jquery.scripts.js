21d4d867c23b0645dc298badc74a85047c55ad63
Henry <henyan94@gmail.com>
2015-09-22 19:36:58 +0200

    Now Sticky header works perfectly! Hurray

--- skin/frontend/default/jonastheme/js/jquery.scripts.js
+++ skin/frontend/default/jonastheme/js/jquery.scripts.js
@@ -1,11 +1,15 @@
 jQuery.noConflict();
 jQuery(document).ready(function (){
+    var headerTop = jQuery('.header-container').outerHeight();
+    console.log(headerTop);
     jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > 1){
+        if (jQuery(this).scrollTop() > 115){
             jQuery('.header-container').addClass("sticky");
+            jQuery('.main-container').css("paddingTop",headerTop)
         }
         else{
             jQuery('.header-container').removeClass("sticky");
+            jQuery('.main-container').css("paddingTop","0")
         }
     });
 });
